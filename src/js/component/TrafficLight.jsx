dad99f6d0e7ece3fe5039650a311b5312922f36b
angeljimenez91 <139405887+angeljimenez91@users.noreply.github.com>
2023-12-25 17:09:35 +0000

    segundo

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -5,50 +5,23 @@ import React, { useState } from "react";
 
 //create your first component
 const TrafficLight = () => {
-  const [red, setRed] = useState('red');
-  const [yellow, setYellow] = useState('yellow');
-  const [green, setGreen] = useState('green');
+  const [selectColor, setSelectedColor] = useState("");
 
-  const changeColor = (color) => {
-    console.log(color)
-  }
 
   return (
     <div className="container">
       <div className="traffic-container">
-        <div 
-          onClick={() => changeColor(red)}
-          style={{
-            width: '125px',
-            height: '125px',
-            borderRadius: '50%',
-            backgroundColor: red ? 'red' : 'darkred',
-            margin: '10px',
-            boxShadow: red ? '0 0 20px 10px rgba(255, 0, 0, 0.7)' : 'none',
-          }}
-        ></div>
-        <div
-          onClick={() => changeColor(yellow)}
-          style={{
-            width: '125px',
-            height: '125px',
-            backgroundColor: yellow ? 'yellow' : 'darkorange',
-            borderRadius: '50%',
-            margin: '10px',
-            boxShadow: yellow ? '0 0 20px 10px rgba(255, 255, 0, 0.7)' : 'none',
-          }}
-        ></div>
-        <div
-          onClick={() => changeColor(green)}
-          style={{
-            width: '125px',
-            height: '125px',
-            backgroundColor: green ? 'green' : 'darkgreen',
-            borderRadius: '50%',
-            margin: '10px',
-            boxShadow: green ? '0 0 20px 10px rgba(0, 255, 0, 0.7)' : 'none',
-          }}
-        ></div>
+        <div className={`red ${selectColor === "red" ? "light-active" : ""}`}
+          onClick={() => setSelectedColor("red")}>
+        </div>
+
+        <div className={`yellow ${selectColor === "yellow" ? "light-active" : ""}`}
+          onClick={() => setSelectedColor("yellow")}>
+        </div>
+
+        <div className={`green ${selectColor === "green" ? "light-active" : ""}`}
+          onClick={() => setSelectedColor("green")}>
+        </div>
       </div>
     </div>
   );
